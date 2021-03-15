76aa310dacccf90f1e066b8a7582b85198356fd2
dlsakbo <56906001+dlsakbo@users.noreply.github.com>
2021-03-15 14:55:51 +0530

    up

--- Voting system/Voting system/Register_Voter.Designer.cs
+++ Voting system/Voting system/Register_Voter.Designer.cs
@@ -48,9 +48,11 @@ namespace Voting_system
             this.CDmName = new System.Windows.Forms.Label();
             this.VTRname = new System.Windows.Forms.TextBox();
             this.Submitbtn = new FontAwesome.Sharp.IconButton();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
             this.Content.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Fingerprint1A)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Fingerprint2A)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
             // 
             // logo
@@ -59,10 +61,10 @@ namespace Voting_system
             this.logo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(80)))), ((int)(((byte)(111)))));
             this.logo.BackgroundImage = global::Voting_system.Properties.Resources.election;
             this.logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.logo.Location = new System.Drawing.Point(1, 1);
-            this.logo.Margin = new System.Windows.Forms.Padding(3, 3, 2, 3);
+            this.logo.Location = new System.Drawing.Point(2, 2);
+            this.logo.Margin = new System.Windows.Forms.Padding(4, 5, 3, 5);
             this.logo.Name = "logo";
-            this.logo.Size = new System.Drawing.Size(798, 126);
+            this.logo.Size = new System.Drawing.Size(1197, 194);
             this.logo.TabIndex = 1;
             // 
             // Content
@@ -84,23 +86,26 @@ namespace Voting_system
             this.Content.Controls.Add(this.CDmName);
             this.Content.Controls.Add(this.VTRname);
             this.Content.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Content.Location = new System.Drawing.Point(1, 159);
+            this.Content.Location = new System.Drawing.Point(2, 245);
+            this.Content.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Content.Name = "Content";
-            this.Content.Size = new System.Drawing.Size(631, 589);
+            this.Content.Size = new System.Drawing.Size(946, 906);
             this.Content.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(46, 122);
+            this.textBox2.Location = new System.Drawing.Point(69, 188);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(366, 20);
+            this.textBox2.Size = new System.Drawing.Size(547, 26);
             this.textBox2.TabIndex = 24;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(46, 331);
+            this.textBox1.Location = new System.Drawing.Point(69, 509);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(366, 20);
+            this.textBox1.Size = new System.Drawing.Size(547, 26);
             this.textBox1.TabIndex = 23;
             // 
             // Tele
@@ -108,27 +113,30 @@ namespace Voting_system
             this.Tele.AutoSize = true;
             this.Tele.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Tele.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Tele.Location = new System.Drawing.Point(42, 294);
+            this.Tele.Location = new System.Drawing.Point(63, 452);
+            this.Tele.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Tele.Name = "Tele";
-            this.Tele.Size = new System.Drawing.Size(129, 25);
+            this.Tele.Size = new System.Drawing.Size(194, 40);
             this.Tele.TabIndex = 22;
             this.Tele.Text = "Telephone No";
             // 
             // Fingerprint1A
             // 
             this.Fingerprint1A.BackColor = System.Drawing.Color.Silver;
-            this.Fingerprint1A.Location = new System.Drawing.Point(27, 453);
+            this.Fingerprint1A.Location = new System.Drawing.Point(40, 697);
+            this.Fingerprint1A.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Fingerprint1A.Name = "Fingerprint1A";
-            this.Fingerprint1A.Size = new System.Drawing.Size(162, 102);
+            this.Fingerprint1A.Size = new System.Drawing.Size(243, 157);
             this.Fingerprint1A.TabIndex = 21;
             this.Fingerprint1A.TabStop = false;
             // 
             // Fingerprint2A
             // 
             this.Fingerprint2A.BackColor = System.Drawing.Color.Silver;
-            this.Fingerprint2A.Location = new System.Drawing.Point(231, 453);
+            this.Fingerprint2A.Location = new System.Drawing.Point(346, 697);
+            this.Fingerprint2A.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Fingerprint2A.Name = "Fingerprint2A";
-            this.Fingerprint2A.Size = new System.Drawing.Size(162, 102);
+            this.Fingerprint2A.Size = new System.Drawing.Size(243, 157);
             this.Fingerprint2A.TabIndex = 20;
             this.Fingerprint2A.TabStop = false;
             // 
@@ -137,9 +145,10 @@ namespace Voting_system
             this.Fingerprint2.AutoSize = true;
             this.Fingerprint2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Fingerprint2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Fingerprint2.Location = new System.Drawing.Point(255, 408);
+            this.Fingerprint2.Location = new System.Drawing.Point(382, 628);
+            this.Fingerprint2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Fingerprint2.Name = "Fingerprint2";
-            this.Fingerprint2.Size = new System.Drawing.Size(100, 21);
+            this.Fingerprint2.Size = new System.Drawing.Size(152, 32);
             this.Fingerprint2.TabIndex = 18;
             this.Fingerprint2.Text = "Fignerprint 2";
             // 
@@ -148,9 +157,10 @@ namespace Voting_system
             this.checkBox2.AutoSize = true;
             this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.checkBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.checkBox2.Location = new System.Drawing.Point(107, 251);
+            this.checkBox2.Location = new System.Drawing.Point(160, 386);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(48, 24);
+            this.checkBox2.Size = new System.Drawing.Size(71, 33);
             this.checkBox2.TabIndex = 17;
             this.checkBox2.Text = "No";
             this.checkBox2.UseVisualStyleBackColor = true;
@@ -160,18 +170,20 @@ namespace Voting_system
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.checkBox1.Location = new System.Drawing.Point(46, 251);
+            this.checkBox1.Location = new System.Drawing.Point(69, 386);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(56, 24);
+            this.checkBox1.Size = new System.Drawing.Size(81, 33);
             this.checkBox1.TabIndex = 16;
             this.checkBox1.Text = "Yes";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(46, 191);
+            this.dateTimePicker1.Location = new System.Drawing.Point(69, 294);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(298, 26);
             this.dateTimePicker1.TabIndex = 15;
             // 
             // Fingerprint1
@@ -179,9 +191,10 @@ namespace Voting_system
             this.Fingerprint1.AutoSize = true;
             this.Fingerprint1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Fingerprint1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Fingerprint1.Location = new System.Drawing.Point(40, 408);
+            this.Fingerprint1.Location = new System.Drawing.Point(60, 628);
+            this.Fingerprint1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Fingerprint1.Name = "Fingerprint1";
-            this.Fingerprint1.Size = new System.Drawing.Size(100, 21);
+            this.Fingerprint1.Size = new System.Drawing.Size(152, 32);
             this.Fingerprint1.TabIndex = 13;
             this.Fingerprint1.Text = "Fignerprint 1";
             // 
@@ -190,9 +203,10 @@ namespace Voting_system
             this.Address.AutoSize = true;
             this.Address.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Address.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Address.Location = new System.Drawing.Point(39, 227);
+            this.Address.Location = new System.Drawing.Point(58, 349);
+            this.Address.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Address.Name = "Address";
-            this.Address.Size = new System.Drawing.Size(378, 25);
+            this.Address.Size = new System.Drawing.Size(568, 40);
             this.Address.TabIndex = 11;
             this.Address.Text = "Please state if your are Citizen of Sri lanka ?";
             // 
@@ -201,9 +215,10 @@ namespace Voting_system
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(41, 156);
+            this.label4.Location = new System.Drawing.Point(62, 240);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(121, 25);
+            this.label4.Size = new System.Drawing.Size(184, 40);
             this.label4.TabIndex = 8;
             this.label4.Text = "Date Of Birth";
             // 
@@ -212,9 +227,10 @@ namespace Voting_system
             this.Party.AutoSize = true;
             this.Party.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Party.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Party.Location = new System.Drawing.Point(39, 81);
+            this.Party.Location = new System.Drawing.Point(58, 125);
+            this.Party.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Party.Name = "Party";
-            this.Party.Size = new System.Drawing.Size(273, 25);
+            this.Party.Size = new System.Drawing.Size(415, 40);
             this.Party.TabIndex = 6;
             this.Party.Text = "National Idemtity Card number";
             // 
@@ -223,17 +239,19 @@ namespace Voting_system
             this.CDmName.AutoSize = true;
             this.CDmName.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CDmName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.CDmName.Location = new System.Drawing.Point(41, 11);
+            this.CDmName.Location = new System.Drawing.Point(62, 17);
+            this.CDmName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.CDmName.Name = "CDmName";
-            this.CDmName.Size = new System.Drawing.Size(166, 25);
+            this.CDmName.Size = new System.Drawing.Size(250, 40);
             this.CDmName.TabIndex = 2;
             this.CDmName.Text = "Full name of Voter";
             // 
             // VTRname
             // 
-            this.VTRname.Location = new System.Drawing.Point(46, 45);
+            this.VTRname.Location = new System.Drawing.Point(69, 69);
+            this.VTRname.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.VTRname.Name = "VTRname";
-            this.VTRname.Size = new System.Drawing.Size(366, 20);
+            this.VTRname.Size = new System.Drawing.Size(547, 26);
             this.VTRname.TabIndex = 0;
             // 
             // Submitbtn
@@ -246,29 +264,37 @@ namespace Voting_system
             this.Submitbtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.Submitbtn.IconSize = 22;
             this.Submitbtn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.Submitbtn.Location = new System.Drawing.Point(706, 717);
+            this.Submitbtn.Location = new System.Drawing.Point(1059, 1103);
+            this.Submitbtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Submitbtn.Name = "Submitbtn";
-            this.Submitbtn.Size = new System.Drawing.Size(93, 31);
+            this.Submitbtn.Size = new System.Drawing.Size(140, 48);
             this.Submitbtn.TabIndex = 13;
             this.Submitbtn.Text = "Submit";
             this.Submitbtn.UseVisualStyleBackColor = false;
             // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
             // Register_Voter
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(80)))), ((int)(((byte)(111)))));
-            this.ClientSize = new System.Drawing.Size(800, 749);
+            this.ClientSize = new System.Drawing.Size(1200, 1050);
             this.Controls.Add(this.Submitbtn);
             this.Controls.Add(this.Content);
             this.Controls.Add(this.logo);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Register_Voter";
             this.Text = "Register_Voter";
             this.Content.ResumeLayout(false);
             this.Content.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Fingerprint1A)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Fingerprint2A)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -293,5 +319,6 @@ namespace Voting_system
         private System.Windows.Forms.Label Tele;
         private System.Windows.Forms.PictureBox Fingerprint1A;
         private System.Windows.Forms.PictureBox Fingerprint2A;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
     }
 }
