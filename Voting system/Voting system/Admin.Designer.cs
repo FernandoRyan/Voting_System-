6b358fcb26ea127271ab5b5f9906ab025f4a1b95
Nethi-perera <62004540+Nethi-perera@users.noreply.github.com>
2021-03-16 22:05:44 +0530

    admin

--- Voting system/Voting system/Admin.Designer.cs
+++ Voting system/Voting system/Admin.Designer.cs
@@ -51,8 +51,9 @@ namespace Voting_system
             this.panel1.Controls.Add(this.logo);
             this.panel1.Controls.Add(this.Cntpanel);
             this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(805, 462);
+            this.panel1.Size = new System.Drawing.Size(1073, 569);
             this.panel1.TabIndex = 0;
             // 
             // logo
@@ -61,10 +62,10 @@ namespace Voting_system
             this.logo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(80)))), ((int)(((byte)(111)))));
             this.logo.BackgroundImage = global::Voting_system.Properties.Resources.election;
             this.logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.logo.Location = new System.Drawing.Point(22, 3);
-            this.logo.Margin = new System.Windows.Forms.Padding(3, 3, 2, 3);
+            this.logo.Location = new System.Drawing.Point(29, 4);
+            this.logo.Margin = new System.Windows.Forms.Padding(4, 4, 3, 4);
             this.logo.Name = "logo";
-            this.logo.Size = new System.Drawing.Size(765, 105);
+            this.logo.Size = new System.Drawing.Size(1020, 129);
             this.logo.TabIndex = 3;
             // 
             // Cntpanel
@@ -77,9 +78,10 @@ namespace Voting_system
             this.Cntpanel.Controls.Add(this.lgAdmin);
             this.Cntpanel.Controls.Add(this.PWD);
             this.Cntpanel.Controls.Add(this.uName);
-            this.Cntpanel.Location = new System.Drawing.Point(22, 114);
+            this.Cntpanel.Location = new System.Drawing.Point(29, 140);
+            this.Cntpanel.Margin = new System.Windows.Forms.Padding(4);
             this.Cntpanel.Name = "Cntpanel";
-            this.Cntpanel.Size = new System.Drawing.Size(765, 323);
+            this.Cntpanel.Size = new System.Drawing.Size(1020, 398);
             this.Cntpanel.TabIndex = 2;
             // 
             // LoginBtn
@@ -90,20 +92,23 @@ namespace Voting_system
             this.LoginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.LoginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LoginBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.LoginBtn.Location = new System.Drawing.Point(294, 214);
+            this.LoginBtn.Location = new System.Drawing.Point(392, 263);
+            this.LoginBtn.Margin = new System.Windows.Forms.Padding(4);
             this.LoginBtn.Name = "LoginBtn";
-            this.LoginBtn.Size = new System.Drawing.Size(141, 42);
+            this.LoginBtn.Size = new System.Drawing.Size(188, 52);
             this.LoginBtn.TabIndex = 3;
             this.LoginBtn.Text = "Login";
             this.LoginBtn.UseVisualStyleBackColor = false;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
             // 
             // lgAdmin
             // 
             this.lgAdmin.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lgAdmin.Image = global::Voting_system.Properties.Resources.Admin;
-            this.lgAdmin.Location = new System.Drawing.Point(316, 32);
+            this.lgAdmin.Location = new System.Drawing.Point(421, 39);
+            this.lgAdmin.Margin = new System.Windows.Forms.Padding(4);
             this.lgAdmin.Name = "lgAdmin";
-            this.lgAdmin.Size = new System.Drawing.Size(100, 69);
+            this.lgAdmin.Size = new System.Drawing.Size(133, 85);
             this.lgAdmin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.lgAdmin.TabIndex = 2;
             this.lgAdmin.TabStop = false;
@@ -111,31 +116,39 @@ namespace Voting_system
             // PWD
             // 
             this.PWD.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.PWD.Location = new System.Drawing.Point(241, 179);
+            this.PWD.Location = new System.Drawing.Point(321, 220);
+            this.PWD.Margin = new System.Windows.Forms.Padding(4);
             this.PWD.Name = "PWD";
             this.PWD.PasswordChar = '*';
-            this.PWD.Size = new System.Drawing.Size(247, 20);
+            this.PWD.Size = new System.Drawing.Size(328, 22);
             this.PWD.TabIndex = 1;
             this.PWD.Text = "Password";
+            this.PWD.Enter += new System.EventHandler(this.txtPassEnter);
+            this.PWD.Leave += new System.EventHandler(this.txtPassLeave);
             // 
             // uName
             // 
             this.uName.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.uName.Location = new System.Drawing.Point(241, 144);
+            this.uName.Location = new System.Drawing.Point(321, 177);
+            this.uName.Margin = new System.Windows.Forms.Padding(4);
             this.uName.Name = "uName";
-            this.uName.Size = new System.Drawing.Size(247, 20);
+            this.uName.Size = new System.Drawing.Size(328, 22);
             this.uName.TabIndex = 0;
             this.uName.Text = "Username";
+            this.uName.TextChanged += new System.EventHandler(this.uName_TextChanged);
+            this.uName.Leave += new System.EventHandler(this.txtUserLeave);
             // 
             // Admin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Admin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin";
             this.panel1.ResumeLayout(false);
             this.Cntpanel.ResumeLayout(false);
